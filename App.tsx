05e9373a14fb2e4aec98bfdf2f158990c2ca7a96
Island <islandhuynh@gmail.com>
2021-05-03 20:45:34 -0400

    testing commit

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Initial Change</Text>
+      <Text>Testing commit</Text>
       <StatusBar style="auto" />
     </View>
   );
